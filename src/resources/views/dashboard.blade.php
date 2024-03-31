fdfea95951a9158c920493cf54a255642dae11cb
Shinsei <murakami.shinsei.zn@gmail.com>
2024-03-31 12:30:01 +0900

    環境判別/実行環境について

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -9,7 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    {{ '実行環境：' . app()->environment() }}
+                    @if (app()->isProduction())
+                        <p>本番環境だよー</p>
+                    @else
+                        <p>本番環境ではないよー</p>
+                    @endif
                 </div>
             </div>
         </div>
