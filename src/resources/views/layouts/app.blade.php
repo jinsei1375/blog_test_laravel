11878df101090fed06fd602cf38fd974be3efdc7
Shinsei <murakami.shinsei.zn@gmail.com>
2024-12-06 08:58:08 +0900

    wip

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="/css/app.css">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -30,5 +30,4 @@
             </main>
         </div>
     </body>
-    <script src="/js/app.js"></script>
 </html>
